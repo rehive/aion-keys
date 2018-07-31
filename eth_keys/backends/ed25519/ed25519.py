a2b2e9883fa38863adedfc43980ab33de2e831af
Mwangi <8320816+kidynamit@users.noreply.github.com>
2018-07-31 22:11:06 +0200

    implementation of ed25519 pulled from vburetin/ed25519.py

--- eth_keys/backends/ed25519/ed25519.py
+++ eth_keys/backends/ed25519/ed25519.py
@@ -1,136 +1,264 @@
-#!/usr/bin/python
+from sha3 import sha3_512 as SHA3512
 
-import hashlib
+# http://ed25519.cr.yp.to/ed25519-20110926.pdf
 
-b = 256
-q = 2 ** 255 - 19
-l = 2 ** 252 + 27742317777372353535851937790883648493
-
-
-def H(m):
-    return hashlib.sha512(m).digest()
-
-
-def expmod(b, e, m):
-    if e == 0:
-        return 1
-    t = expmod(b, e / 2, m) ** 2 % m
-    if e & 1:
-        t = t * b % m
-    return t
+BITS = 256
+Q = 2**255 - 19
+L = 2**252 + 27742317777372353535851937790883648493
+D = 37095705934669439343138083508754565189542113879843219016388785533085940283555
+By = 46316835694926478169428394003475163141307993866256225615783033603165251855960L
+Bx = 15112221349535400772501151409588531511454012693041857206046113283949847762202L
+B = (Bx, By)
 
 
-def inv(x):
-    return expmod(x, q - 2, q)
+# Modular inverse
+def inv(a, n):
+    if a == 0:
+        return 0
+    lm, hm = 1, 0
+    low, high = a % n, n
+    while low > 1:
+        r = high//low
+        nm, new = hm-lm*r, high-low*r
+        lm, low, hm, high = nm, new, lm, low
+    return lm % n
 
 
-d = -121665 * inv(121666)
-I = expmod(2, (q - 1) / 4, q)
+# http://eli.thegreenplace.net/2009/03/07/computing-modular-square-roots-in-python/
+def modular_sqrt(a, p):
+    """ Find a quadratic residue (mod p) of 'a'. p
+        must be an odd prime.
+
+        Solve the congruence of the form:
+            x^2 = a (mod p)
+        And returns x. Note that p - x is also a root.
+
+        0 is returned is no square root exists for
+        these a and p.
+
+        The Tonelli-Shanks algorithm is used (except
+        for some simple cases in which the solution
+        is known from an identity). This algorithm
+        runs in polynomial time (unless the
+        generalized Riemann hypothesis is false).
+    """
+    # Simple cases
+    #
+    if legendre_symbol(a, p) != 1:
+        return 0
+    elif a == 0:
+        return 0
+    elif p == 2:
+        return p
+    elif p % 4 == 3:
+        return pow(a, (p + 1) / 4, p)
+
+    # Partition p-1 to s * 2^e for an odd s (i.e.
+    # reduce all the powers of 2 from p-1)
+    #
+    s = p - 1
+    e = 0
+    while s % 2 == 0:
+        s /= 2
+        e += 1
+
+    # Find some 'n' with a legendre symbol n|p = -1.
+    # Shouldn't take long.
+    #
+    n = 2
+    while legendre_symbol(n, p) != -1:
+        n += 1
+
+    # Here be dragons!
+    # Read the paper "Square roots from 1; 24, 51,
+    # 10 to Dan Shanks" by Ezra Brown for more
+    # information
+    #
+
+    # x is a guess of the square root that gets better
+    # with each iteration.
+    # b is the "fudge factor" - by how much we're off
+    # with the guess. The invariant x^2 = ab (mod p)
+    # is maintained throughout the loop.
+    # g is used for successive powers of n to update
+    # both a and b
+    # r is the exponent - decreases with each update
+    #
+    x = pow(a, (s + 1) / 2, p)
+    b = pow(a, s, p)
+    g = pow(n, s, p)
+    r = e
+
+    while True:
+        t = b
+        m = 0
+        for m in xrange(r):
+            if t == 1:
+                break
+            t = pow(t, 2, p)
+
+        if m == 0:
+            assert pow(x, 2, p) == a
+            return x
+
+        gs = pow(g, 2 ** (r - m - 1), p)
+        g = (gs * gs) % p
+        x = (x * gs) % p
+        b = (b * g) % p
+        r = m
 
 
-def xrecover(y):
-    xx = (y * y - 1) * inv(d * y * y + 1)
-    x = expmod(xx, (q + 3) / 8, q)
-    if (x * x - xx) % q != 0:
-        x = x * I % q
-    if x % 2 != 0:
-        x = q - x
-    return x
+def legendre_symbol(a, p):
+    """ Compute the Legendre symbol a|p using
+        Euler's criterion. p is a prime, a is
+        relatively prime to p (if p divides
+        a, then a|p = 0)
+
+        Returns 1 if a has a square root modulo
+        p, -1 otherwise.
+    """
+    ls = pow(a, (p - 1) / 2, p)
+    return -1 if ls == p - 1 else ls
 
 
-By = 4 * inv(5)
-Bx = xrecover(By)
-B = [Bx % q, By % q]
+# Check if a point is on the curve
+def is_on_curve(p):
+    return (-p[0] ** 2 + p[1] ** 2) % Q == (1 + D * p[0] ** 2 * p[1] ** 2) % Q
 
 
-def edwards(P, Q):
-    x1 = P[0]
-    y1 = P[1]
-    x2 = Q[0]
-    y2 = Q[1]
-    x3 = (x1 * y2 + x2 * y1) * inv(1 + d * x1 * x2 * y1 * y2)
-    y3 = (y1 * y2 + x1 * x2) * inv(1 - d * x1 * x2 * y1 * y2)
-    return [x3 % q, y3 % q]
+# Add two points
+def add(p, q):
+    den_factor = (D * p[0] * p[1] * q[0] * q[1]) % Q
+    x_num = (p[0] * q[1] + p[1] * q[0]) % Q
+    y_num = (p[0] * q[0] + p[1] * q[1]) % Q
+    o = ((x_num * inv(1 + den_factor, Q)) % Q,
+         (y_num * inv(1 - den_factor, Q)) % Q)
+    return o
 
 
-def scalarmult(P, e):
-    if e == 0:
-        return [0, 1]
-    Q = scalarmult(P, e / 2)
-    Q = edwards(Q, Q)
-    if e & 1:
-        Q = edwards(Q, P)
-    return Q
+# Multiply a point by a scalar using the double-and-add algorithm
+def multiply(p, n, adder=add):
+    if n == 0:
+        return (0, 1)
+    elif n == 1:
+        return p
+    else:
+        x = multiply(adder(p, p), n/2, adder)
+        if n % 2:
+            x = adder(x, p)
+        return x
 
 
-def encodeint(y):
-    bits = [y >> i & 1 for i in range(b)]
-    return ''.join([chr(sum([bits[i * 8 + j] << j for j in range(8)]))
-                   for i in range(b / 8)])
+# Convert a point to extended format, see
+# http://eprint.iacr.org/2008/522.pdf
+def to_extended(p):
+    return (p[0], p[1], p[0] * p[1] % Q, 1)
 
 
-def encodepoint(P):
-    x = P[0]
-    y = P[1]
-    bits = [y >> i & 1 for i in range(b - 1)] + [x & 1]
-    return ''.join([chr(sum([bits[i * 8 + j] << j for j in range(8)]))
-                   for i in range(b / 8)])
+# Convert back from extended format
+def from_extended(p):
+    zinv = inv(p[3], Q)
+    return (p[0] * zinv % Q, p[1] * zinv % Q)
 
 
-def bit(h, i):
-    return ord(h[i / 8]) >> i % 8 & 1
+# Add two points in extended format. The reason this algorithm is
+# used is that modular inverses are very expensive, and the extended
+# form essentially keeps track of a denominator and numerators
+# separately, allowing all the required divisions to be "batched"
+# together at the end with only one inverse (instead of an average
+# of 384 inverse operations using the normal algorithm)
+def add_extended(p, q):
+    _A = p[0] * q[0] % Q
+    _B = p[1] * q[1] % Q
+    _C = D * p[2] * q[2] % Q
+    _D = p[3] * q[3] % Q
+    _E = ((p[0] + p[1]) * (q[0] + q[1]) - _A - _B) % Q
+    _F = _D - _C
+    _G = _D + _C
+    _H = _B + _A  # _B - a * _A, but a = -1
+    return (_E * _F % Q, _G * _H % Q, _E * _H % Q, _F * _G % Q)
 
 
-def publickey(sk):
-    h = H(sk)
-    a = 2 ** (b - 2) + sum(2 ** i * bit(h, i) for i in range(3, b - 2))
-    A = scalarmult(B, a)
-    return encodepoint(A)
+def fast_multiply(p, n):
+    return from_extended(multiply(to_extended(p), n, adder=add_extended))
 
 
-def Hint(m):
-    h = H(m)
-    return sum(2 ** i * bit(h, i) for i in range(2 * b))
+def sha3_512(x):
+    return SHA3512(x).digest()
 
 
-def signature(m, sk, pk):
-    h = H(sk)
-    a = 2 ** (b - 2) + sum(2 ** i * bit(h, i) for i in range(3, b - 2))
-    r = Hint(''.join([h[i] for i in range(b / 8, b / 4)]) + m)
-    R = scalarmult(B, r)
-    S = (r + Hint(encodepoint(R) + pk + m) * a) % l
-    return encodepoint(R) + encodeint(S)
+# Converts a number into encoded form (for hashing)
+def encode32(i):
+    o = ''
+    for _ in range(32):
+        o += chr(i % 256)
+        i /= 256
+    return o
 
 
-def isoncurve(P):
-    x = P[0]
-    y = P[1]
-    return (-x * x + y * y - 1 - d * x * x * y * y) % q == 0
+# Converts back from encoded form
+def decode_int(s):
+    o = 0
+    for c in s[::-1]:
+        o = o * 256 + ord(c)
+    return o
 
 
-def decodeint(s):
-    return sum(2 ** i * bit(s, i) for i in range(0, b))
+# Standard-form encoding for a public key
+def encode_pubkey(p):
+    v = p[1]
+    if p[0] > Q - p[0]:
+        v |= 2**255
+    return encode32(v)
 
 
-def decodepoint(s):
-    y = sum(2 ** i * bit(s, i) for i in range(0, b - 1))
-    x = xrecover(y)
-    if x & 1 != bit(s, b - 1):
-        x = q - x
-    P = [x, y]
-    if not isoncurve(P):
-        raise Exception('decoding point that is not on curve')
-    return P
+def x_from_y(y):
+    # -x^2 + y^2 = 1 + d * x^2 * y^2
+    # implies
+    # (y^2 - 1) / (d * y^2 + 1) = x^2
+    print y
+    nom = (y*y-1) % Q
+    den = (D*y*y+1) % Q
+    return modular_sqrt((nom * inv(den, Q)) % Q, Q)
 
 
-def checkvalid(s, m, pk):
-    if len(s) != b / 4:
-        raise Exception('signature length is wrong')
-    if len(pk) != b / 8:
-        raise Exception('public-key length is wrong')
-    R = decodepoint(s[0:b / 8])
-    A = decodepoint(pk)
-    S = decodeint(s[b / 8:b / 4])
-    h = Hint(encodepoint(R) + pk + m)
-    if scalarmult(B, S) != edwards(R, scalarmult(A, h)):
-        raise Exception('signature does not pass verification')
+def decode_pubkey(p):
+    v = decode_int(p)
+    y = v % 2**255
+    x = x_from_y(y)
+    if (x > Q - x) ^ (v >> 255):
+        x = Q - x
+    return (x, y)
+
+
+# Converts a privkey into a pubkey
+def privtopub(k):
+    h = sha3_512(k)
+    a = 2 ** (BITS - 2) + (decode_int(h[:32]) % 2 ** (BITS - 2))
+    a -= (a % 8)
+    return fast_multiply(B, a)
+
+
+# Signature algorithm
+def sign(k, m):
+    h = sha3_512(k)
+    a = 2 ** (BITS - 2) + (decode_int(h[:32]) % 2 ** (BITS - 2))
+    a -= (a % 8)
+    A = fast_multiply(B, a)
+    r = decode_int(h[32:])
+    R = fast_multiply(B, r)
+    h2 = sha3_512(encode_pubkey(R) + encode_pubkey(A) + m)
+    s = (r + decode_int(h2) * a) % L
+    return encode_pubkey(R) + encode32(s)
+
+
+# Verification algorithm
+def verify(pub, sig, m):
+    R = decode_pubkey(sig[:32])
+    s = decode_int(sig[32:])
+    if s >= L:
+        return False
+    A = encode_pubkey(pub)
+    h2 = sha3_512(sig[:32] + A + m)
+    assert is_on_curve(R)
+    return fast_multiply(B, 8 * s) == add(fast_multiply(R, 8), fast_multiply(pub, 8 * decode_int(h2)))
